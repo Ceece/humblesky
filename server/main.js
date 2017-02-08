adddd618db1ad4cce1d7de7e9bf8d05e0204669e
Teerapong Manakul <teerapong.m@zanrook.com>
2017-02-09 01:03:43 +0700

    More template

--- server/main.js
+++ server/main.js
@@ -1,11 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-var ncp = require('ncp');
-var fs = require('fs');
-
 Meteor.startup(() => {
-    if (!fs.existsSync(`${process.env.PWD}/public`)) {
-        fs.mkdirSync(`${process.env.PWD}/public`);
-    }
-    ncp(`${process.env.PWD}/node_modules/creative/img`, `${process.env.PWD}/public/img`);
+  // code to run on server at startup
 });
