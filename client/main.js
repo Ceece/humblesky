7cc3cfd9ceead83527b8fbbebfc246141855f2ba
Teerapong Manakul <teerapong.m@zanrook.com>
2017-02-08 16:04:11 +0700

    Ghost button Layout LESS

--- client/main.js
+++ client/main.js
@@ -2,7 +2,6 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import './../node_modules/creative/css/creative.min.css';
 import './../node_modules/jquery.easing/jquery.easing.min.js';
 import './../node_modules/creative/vendor/scrollreveal/scrollreveal.min.js';
 import './../node_modules/creative/vendor/magnific-popup/jquery.magnific-popup.min.js';
--- server/main.js
+++ server/main.js
@@ -4,9 +4,8 @@ var ncp = require('ncp');
 var fs = require('fs');
 
 Meteor.startup(() => {
-    if (!fs.existsSync(`${process.env.PWD}/public`))
+    if (!fs.existsSync(`${process.env.PWD}/public`)) {
         fs.mkdirSync(`${process.env.PWD}/public`);
-    if (!fs.existsSync(`${process.env.PWD}/img`))
-        fs.mkdirSync(`${process.env.PWD}/img`);
+    }
     ncp(`${process.env.PWD}/node_modules/creative/img`, `${process.env.PWD}/public/img`);
 });
