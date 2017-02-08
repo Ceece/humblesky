9ca119a8e6435655bcd233488202ae7c92fb45f0
Teerapong Manakul <teerapong.m@zanrook.com>
2017-02-08 10:14:45 +0700

    Import template

--- client/main.js
+++ client/main.js
@@ -2,21 +2,11 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './../node_modules/creative/css/creative.min.css';
+import './../node_modules/jquery.easing/jquery.easing.min.js';
+import './../node_modules/creative/vendor/scrollreveal/scrollreveal.min.js';
+import './../node_modules/creative/vendor/magnific-popup/jquery.magnific-popup.min.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.body.onRendered(function onRendered() {
+    import './../node_modules/creative/js/creative.min.js';
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
+var ncp = require('ncp');
+var fs = require('fs');
+
 Meteor.startup(() => {
-  // code to run on server at startup
+    if (!fs.existsSync(`${process.env.PWD}/public`))
+        fs.mkdirSync(`${process.env.PWD}/public`);
+    if (!fs.existsSync(`${process.env.PWD}/img`))
+        fs.mkdirSync(`${process.env.PWD}/img`);
+    ncp(`${process.env.PWD}/node_modules/creative/img`, `${process.env.PWD}/public/img`);
 });
